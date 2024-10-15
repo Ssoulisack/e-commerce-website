3981962ca5afd5132895ef33c93b496a6ee09cda
Ssoulisack <vt.utd2@gmail.com>
2024-10-16 01:05:58 +0700

    create admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import { Routes, Route } from 'react-router-dom'
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'
+import Login from './components/Login'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+
+  const [token, setToken] = useState('');
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='bg-gray-50 min-h-screen'>
+      {token === ""
+        ? <Login />
+        : <>
+          <Navbar />
+          <hr />
+          <div className='flex w-full'>
+            <Sidebar />
+            <div className='w-[70%] mx-auto ml-[max-(5vw, 25px)] my-8 text-gray-600 text-base'>
+              <Routes>
+                <Route path='/add' element={<Add />} />
+                <Route path='/list' element={<List />} />
+                <Route path='/orders' element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      }
+    </div>
   )
 }
 
---
+++ admin/src/components/Login.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onSubmitHandler = async (e) => {
+        try {
+            e.preventDefault();
+            
+
+        } catch (error) {
+            
+        }
+    }
+
+  return (
+    <div className='min-h-screen flex items-center justify-center w-full'>
+        <div className='bg-white shadow-md rounded-lg px-8 py-6 max-w-md'>
+            <h1 className='text-2xl font-bold mb-4'>Admin Panel</h1>
+            <form onSubmit={onSubmitHandler}>
+                <div>
+                    <p className='text-sm font-medium text-gray-700 mb-2'>Email Address</p>
+                    <input onChange={(e)=>setEmail(e.target.value)} value={email} className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="email" placeholder='your@email.com' required />
+                </div>
+                <div>
+                    <p className='text-sm font-medium text-gray-700 mb-2'>Password</p>
+                    <input onChange={(e)=>setPassword(e.target.value)} value={password} className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="password" placeholder='Enter your password' required />
+                </div>
+                <button className='mt-2 w-full py-2 px-4 rounded-md text-white bg-black' type='submit'>Login</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Login
