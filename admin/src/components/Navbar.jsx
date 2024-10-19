ec0198c42ef31ab9799a2440273c6661fbf59dd5
souliasck <m@Soulisack.local>
2024-10-19 23:24:53 +0700

    create function addproduct and remove at admin panel

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { assets } from "../assets/assets";
-const Navbar = () => {
+const Navbar = ({setToken}) => {
   return (
     <div className='flex items-center py-2 px-[4%] justify-between'>
         <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
-        <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+        <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
 }
--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,8 +1,76 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { backendUrl, currency } from '../App';
+import { toast } from 'react-toastify';
 
-const List = () => {
+const List = ({ token }) => {
+
+  const [list, setList] = useState([]);
+  const fetchList = async () => {
+    try {
+
+      const response = await axios.get(backendUrl + "/api/product/list")
+      if (response.data.success) {
+        setList(response.data.products);
+      } else {
+        toast.error(response.data.message);
+      }
+
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  }
+
+  // Remove product function
+  const removeProduct = async (id) => {
+    try {
+
+      const response = await axios.post(backendUrl + "/api/product/remove", { id }, { headers: { token } })
+      if (response.data.success) {
+        toast.success(response.data.message)
+        await fetchList();
+      } else {
+        toast.error(response.data.message)
+      }
+
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchList();
+  }, [])
+
   return (
-    <div>List</div>
+    <>
+      <p className='mb-2'>All Products List</p>
+
+      {/* List Table Title */}
+      <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-slate-100 text-sm'>
+        <strong>image</strong>
+        <strong>Name</strong>
+        <strong>Category</strong>
+        <strong>Price</strong>
+        <strong className='text-center'>Action</strong>
+      </div>
+
+      {/* Product List */}
+      {
+        list.map((item, index) => (
+          <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border text-sm' key={index}>
+            <img className='w-12' src={item.image[0]} alt="" />
+            <p>{item.name}</p>
+            <p>{item.category}</p>
+            <p>{currency}{item.price}</p>
+            <p onClick={() => removeProduct(item._id)} className='text-right md:text-center cursor-pointer text-lg'>X</p>
+          </div>
+        ))
+      }
+
+    </>
   )
 }
 
